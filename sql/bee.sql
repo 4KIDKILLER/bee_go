88c07d7c6259617e92200c5a5a74edb48363845c
s1mple <s1mple@email.com>
2026-07-30 20:57:19 +0800

    feat:转储数据库结构

--- sql/bee.sql
+++ sql/bee.sql
@@ -11,18 +11,44 @@
  Target Server Version : 80046 (8.0.46)
  File Encoding         : 65001
 
- Date: 24/07/2026 17:58:47
+ Date: 30/07/2026 20:56:56
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for bee_file
+-- ----------------------------
+DROP TABLE IF EXISTS `bee_file`;
+CREATE TABLE `bee_file` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT '表自增id',
+  `parent_id` varchar(50) NOT NULL COMMENT '所属文件夹id',
+  `file_id` varchar(50) NOT NULL COMMENT '文件id',
+  `user_id` int NOT NULL COMMENT '关联用户id',
+  `file_original_name` varchar(50) DEFAULT NULL COMMENT '原文件名',
+  `file_name` varchar(50) DEFAULT NULL COMMENT '文件名称',
+  `file_size` decimal(9,2) DEFAULT NULL COMMENT '文件大小,单位:Kb',
+  `file_path` varchar(100) DEFAULT NULL COMMENT '存储地址',
+  `file_type` tinyint DEFAULT NULL COMMENT '类型:1文件夹 2文件',
+  `tags` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '标签',
+  `cover_1` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件夹封面1，file_type=2时可用',
+  `cover_2` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件夹封面2，file_type=2时可用',
+  `cover_3` varchar(200) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '文件夹封面3，file_type=2时可用',
+  `remark` varchar(100) DEFAULT NULL COMMENT '备注',
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `status` tinyint DEFAULT '1' COMMENT '文件状态:1正常 2禁用',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
 -- Table structure for bee_user
 -- ----------------------------
 DROP TABLE IF EXISTS `bee_user`;
 CREATE TABLE `bee_user` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'user表自增id',
+  `id` int NOT NULL AUTO_INCREMENT COMMENT '表自增id',
+  `user_id` int NOT NULL COMMENT '用户id',
   `username` varchar(50) DEFAULT NULL COMMENT '用户名',
   `password` varchar(50) DEFAULT NULL COMMENT '密码',
   `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP COMMENT '注册时间',
@@ -30,12 +56,6 @@ CREATE TABLE `bee_user` (
   `avatar` varchar(100) DEFAULT NULL COMMENT '头像',
   `status` tinyint unsigned NOT NULL DEFAULT '1' COMMENT '状态: 1正常 2禁用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- ----------------------------
--- Records of bee_user
--- ----------------------------
-BEGIN;
-COMMIT;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
